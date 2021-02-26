0230f67218c977e7ced49338b77f3dc8da4f36bb
ShohanurRahmanShohan <78851871+ShohanurRahmanShohan@users.noreply.github.com>
2021-02-26 14:39:25 +0600

    Add files via upload

---
+++ Logic.js
@@ -0,0 +1,29 @@
+function cal(){
+   var  hight = prompt (" your hight in inches" ) ; 
+   var weight= prompt (" your weight in kg");
+   var c = hight/12
+   var i = c/3.2808399
+   var h = i*i
+   var num = weight/h ;
+     if (num < 18.5){
+       document.getElementById("aw").innerHTML = "Underweight ~"+num + " :     A BMI of less than 18.5 indicates that you are underweight, so you may need to put on some weight. You are recommended to ask your doctor or a dietitian for advice.";
+      }
+      else if (num >= 18.5 && num <= 24.9  ) {
+         document.getElementById("aw").innerHTML = "Ideal ~"+num + "   :  A BMI of 18.5–24.9 indicates that you are at a healthy weight for your height. By maintaining a healthy weight, you lower your risk of developing serious health problems.";
+      }
+      else if (num >= 25 && num <= 29.9 ) {
+          document.getElementById("aw").innerHTML = "Overweight ~" +num + "      : A BMI of 25–29.9 indicates that you are slightly overweight. You may be advised to lose some weight for health reasons. You are recommended to talk to your doctor or a dietitian for advice.";
+      }
+      else {
+           document.getElementById("aw").innerHTML ="Ndicatess obesity ~" +num +  "        : A BMI of over 30 indicates that you are heavily overweight. Your health may be at risk if you do not lose weight. You are recommended to talk to your doctor or a dietitian for advice.";
+      }
+}
+function fh(){
+  var hi= prompt ("feet : ");
+   var cod = hi*12;
+  document.getElementById("ff").innerHTML = "~"+ cod + `inc`;
+}
+function r (){
+  document.getElementById("ff").innerHTML = " ";
+  document.getElementById("aw").innerHTML = "CLICK THE START FOR START YOUR BMI HERE AND SOME GOOD ADVISE FOR YOU";
+}
