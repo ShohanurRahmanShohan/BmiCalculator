b228a5bb009b110f6a66b01d4ff1e6d95799e9cd
Shohanur Rahman <78851871+ShohanurRahmanShohan@users.noreply.github.com>
2021-04-18 13:34:08 +0600

    Update Logic.js

--- Logic.js
+++ Logic.js
@@ -1,17 +1,17 @@
 function cal(){
    var  hight = prompt (" Your hight in inches ?" ) ; 
    var weight= prompt (" Your weight in kg ?");
-   var c = hight/12
-   var i = c/3.2808399
-   var h = i*i
-   var num = weight/h ;
-     if (num < 18.5){
+   var cm = hight/12
+   var feet_m = cm/3.2808399
+   var Converted_hight = feet_m**2
+   var Result = weight/Converted_hight;
+     if (Result < 18.5){
        document.getElementById("aw").innerHTML = "Underweight ~"+num + " :     A BMI of less than 18.5 indicates that you are underweight, so you may need to put on some weight. You are recommended to ask your doctor or a dietitian for advice.";
       }
-      else if (num >= 18.5 && num <= 24.9  ) {
+      else if (Result >= 18.5 && num <= 24.9  ) {
          document.getElementById("aw").innerHTML = "Ideal ~"+num + "   :  A BMI of 18.5–24.9 indicates that you are at a healthy weight for your height. By maintaining a healthy weight, you lower your risk of developing serious health problems.";
       }
-      else if (num >= 25 && num <= 29.9 ) {
+      else if (Result >= 25 && num <= 29.9 ) {
           document.getElementById("aw").innerHTML = "Overweight ~" +num + "      : A BMI of 25–29.9 indicates that you are slightly overweight. You may be advised to lose some weight for health reasons. You are recommended to talk to your doctor or a dietitian for advice.";
       }
       else {
